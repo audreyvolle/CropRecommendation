bd3bc02b4dd290c9bc65929a5c4af57c58bff704
Audrey Volle <48231908+audreyvolle@users.noreply.github.com>
2023-10-24 12:52:34 -0500

    Add files via upload

---
+++ Visualization/tsne_crop_data.R
@@ -0,0 +1,31 @@
+# Install all the required packages (if not already installed)
+install.packages("Rtsne")
+install.packages("ggplot2")
+
+# Load the required packages
+library(Rtsne)
+library(ggplot2)
+
+# Load the CSV file into a data frame
+your_data <- read.csv("Crop_recommendation.csv")
+
+# Perform t-SNE on your data
+tsne_out <- Rtsne(as.matrix(your_data[, c("N", "P", "K", "temperature")]))
+
+# Create a data frame for t-SNE output
+tsne_plot <- data.frame(x = tsne_out$Y[, 1], y = tsne_out$Y[, 2])
+
+# Add 'label' column from the original data to tsne_plot
+tsne_plot$label <- your_data$label
+
+# Plotting the t-SNE plot using ggplot2
+ggplot(tsne_plot, aes(x = x, y = y, color = label)) +
+  geom_point(size = 3) +
+  theme_minimal() +
+  ggtitle("t-SNE Visualization") +
+  xlab("t-SNE Dimension 1") +
+  ylab("t-SNE Dimension 2")
+
+
+
+
