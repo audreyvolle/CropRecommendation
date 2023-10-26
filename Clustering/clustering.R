dbbb02bcb54a8a8a1d2cc9232fd9bae36273d2b8
audreyvolle <48231908+audreyvolle@users.noreply.github.com>
2023-10-26 11:54:19 -0500

    start clustering

--- Clustering/clustering.R
+++ Clustering/clustering.R
@@ -1,1 +1,12 @@
 # Clustering
+# https://www.r-bloggers.com/2016/01/hierarchical-clustering-in-r-2/
+
+# Load the CSV file into a data frame
+getwd()
+setwd("/Users/audreyvolle/Desktop/School/MachineLearning/CropRecommendation/")
+your_data <- read.csv("Crop_recommendation.csv")
+
+clusters <- hclust(dist(your_data[, 3:4]))
+plot(clusters)
+
+#clusterCut <- cutree(clusters, 3)
